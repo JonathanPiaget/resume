655f88b6c3cc1ba1eda5e154cfc7938c4fdbc6e5
Jonathan Piaget <piagetjonathan@protonmail.com>
2024-12-24 12:25:29 -0500

    Initialize internationalization with inlang

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
 import astroIcon from "astro-icon";
 import { defineConfig } from "astro/config";
+import paraglide from "@inlang/paraglide-astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,5 +24,13 @@ export default defineConfig({
         Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
       },
     }),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
   ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+  },
 });
